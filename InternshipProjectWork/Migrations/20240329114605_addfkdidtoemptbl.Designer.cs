8051ba9c0fcbea8c352f8bde13ba27f4cf46ecb4
tntra <tntra@DESKTOP-F6047KK>
2024-03-29 18:06:18 +0530

    task-3 and department name for each employee

--- InternshipProjectWork/Migrations/20240329114605_addfkdidtoemptbl.Designer.cs
+++ InternshipProjectWork/Migrations/20240329114605_addfkdidtoemptbl.Designer.cs
@@ -4,6 +4,7 @@ using InternshipProjectWork.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InternshipProjectWork.Migrations
 {
     [DbContext(typeof(ProjectDBContext))]
-    partial class ProjectDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240329114605_addfkdidtoemptbl")]
+    partial class addfkdidtoemptbl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -30,10 +33,10 @@ namespace InternshipProjectWork.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("DepartmentID"));
 
-                    b.Property<string>("DeptName")
+                    b.Property<string>("Name")
                         .IsRequired()
                         .HasColumnType("nvarchar(30)")
-                        .HasColumnName("DeptName");
+                        .HasColumnName("Name");
 
                     b.HasKey("DepartmentID");
 
--- InternshipProjectWork/Migrations/20240329122934_UPdeptname.Designer.cs
+++ InternshipProjectWork/Migrations/20240329122934_UPdeptname.Designer.cs
@@ -4,6 +4,7 @@ using InternshipProjectWork.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InternshipProjectWork.Migrations
 {
     [DbContext(typeof(ProjectDBContext))]
-    partial class ProjectDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240329122934_UPdeptname")]
+    partial class UPdeptname
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
